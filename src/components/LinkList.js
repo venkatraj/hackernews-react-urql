5e0edcb7f5e2b8d68e9c0413e0c9ff5afca1d468
unknown <venkat@codinggeek.com>
2020-08-07 18:48:13 +0530

    fetching graphql server data with urql

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import Link from './Link';
+import { useQuery } from 'urql';
+import gql from 'graphql-tag';
 
-const linksToRender = [
+const FEED_QUERY = gql`
   {
-    id: '1',
-    description: 'Prisma turns your database into a GraphQL API 😎',
-    url: 'https://www.prismagraphql.com',
-  },
-  {
-    id: '2',
-    description: 'The best GraphQL client',
-    url: 'https://formidable.com/open-source/urql/',
-  },
-];
+    feed {
+      links {
+        id
+        createdAt
+        description
+        url
+      }
+    }
+  }
+`;
 
-const LinkList = () => (
-  <div>
-    {linksToRender.map((link) => (
-      <Link key={link.id} link={link} />
-    ))}
-  </div>
-);
+const LinkList = () => {
+  const [result] = useQuery({ query: FEED_QUERY });
+  const { data, fetching, error } = result;
+
+  if (fetching) return <div>Fetching...</div>;
+  if (error) return <div>Error...</div>;
+  const linksToRender = data.feed.links;
+
+  return (
+    <div>
+      {linksToRender.map((link) => (
+        <Link key={link.id} link={link} />
+      ))}
+    </div>
+  );
+};
 
 export default LinkList;
