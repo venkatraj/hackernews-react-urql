cc473c1f4ff6f2296b9cd14d303dc9274589315d
unknown <venkat@codinggeek.com>
2020-08-07 17:53:44 +0530

    configure urql client

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import { Provider, Client, defaultExchanges } from 'urql';
+
+const client = new Client({
+  url: 'http://localhost:4000',
+  exchanges: defaultExchanges,
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider value={client}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 
